0eb405cf4b8fa7434fa9952eb7870aa39a14b589
Trolljeger <35817593+Trolljeger@users.noreply.github.com>
2023-02-09 12:19:10 +0300

    Update main.py
    Добавлено вычисление количества слов и предложений в тексте

--- main.py
+++ main.py
@@ -33,14 +33,30 @@ pm_count = 0 # количество символов знаков препина
 for m in punctuation_marks:
     pm_count += str_in.count(m)
 
+# определение числа слов
+word_list = re.split(' |\n|\r|\t', str_in)
 
-word_list = re.split(' |\n|\r|\f|\t', str_in)
+# Удаляем пустые строки
+word_list = [wl_item for wl_item in word_list if '' != wl_item]
 word_count = len(word_list)
+
+# определение числа предложений
+sentence_count = 0
+
+# варианы окончания предложений
+sentence_end = []
+for s1 in '.!?' :
+    for s2 in ' \n\r\t' :
+        sentence_end.append(s1+s2)
+
+for i in sentence_end :
+    sentence_count += str_in.count(i)
+
 
 
 print(f'Общее колическтво символов в фале: == >\t{str_len}')
 print(f'Общее колическтво символов без пробелов: == >\t{str_len - space_count}')
 print(f'Количество символов без знаков препинания: == >\t{str_len - pm_count}')
 print(f'Количество слов в файле: == >\t{word_count}')
-
+print(f'Количество предложений в файле: == >\t{sentence_count}')
 f_in.close()
